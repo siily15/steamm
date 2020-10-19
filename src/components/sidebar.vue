a5c9b0993197348576989adab9e21a83ac91f987
siily <arnokosk15@gmail.com>
2020-10-19 13:59:04 +0300

    uppercase for letters

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,37 +1,57 @@
 <template>
-  <img src="https://steamstore-a.akamaihd.net/public/images//gift/steamcards_promo_02.png" alt="Steam gift cards"/>
-    <div class="text-gray-200 flex flex-col text-left ml-24 mt-1 text-sm">KINKEKAARDID
-        <a href="#" class="text-blue-500 hover:text-white" >Nüüd on olemas</a>
-    </div>
-    <div class="text-gray-200 flex flex-col text-left ml-24 mt-1 text-sm">SOOVITUSED
-        <a href="#" class="text-blue-500 hover:text-white">Sõprade Poolt</a>
-        <a href="#" class="text-blue-500 hover:text-white" >Kuraatorite poolt</a>
-        <a href="#" class="text-blue-500 hover:text-white" >Märksõnad</a>
-        </div>
-    <div class="text-gray-200 flex flex-col text-left ml-24 mt-1 text-sm">AVASTAMISE JÄRJEKORD
-        <a href="#" class="text-blue-500 hover:text-white">Soovitused</a>
-        <a href="#" class="text-blue-500 hover:text-white">uued väljaanded</a>
-    </div>
-    <div class="text-gray-200 flex flex-col text-left ml-24 mt-1 text-sm">KATEGOORIAD
-        <a href="#" class="text-blue-500 hover:text-white">enimmüüdud</a>
-        <a href="#" class="text-blue-500 hover:text-white">uued väljaanded</a>
-        <a href="#" class="text-blue-500 hover:text-white">tulemas</a>
-        <a href="#" class="text-blue-500 hover:text-white">eripakkumised</a>
-        <a href="#" class="text-blue-500 hover:text-white">Virtuaalne reaalsus</a>
-        <a href="#" class="text-blue-500 hover:text-white">kontroller sõbralik</a>
-        </div>
-    <div class="text-gray-200 flex flex-col text-left ml-24 mt-1 text-sm">ŽANRID
-           <a href="#" class="text-blue-500 hover:text-white">tasuta mängida</a>
-        <a href="#" class="text-blue-500 hover:text-white">varajane ligipääs</a>
-        <a href="#" class="text-blue-500 hover:text-white">Tegevus</a>
-        <a href="#" class="text-blue-500 hover:text-white">Seiklus</a>
-        <a href="#" class="text-blue-500 hover:text-white">Juhuslik</a>
-        <a href="#" class="text-blue-500 hover:text-white">inde</a>
-        <a href="#" class="text-blue-500 hover:text-white">Massiivselt mitmikmäng</a>
-        <a href="#" class="text-blue-500 hover:text-white">Võidusõit</a>
-        <a href="#" class="text-blue-500 hover:text-white">RPG</a>
-        <a href="#" class="text-blue-500 hover:text-white">Simulatsioon</a>
-        <a href="#" class="text-blue-500 hover:text-white">Sport</a>
-        <a href="#" class="text-blue-500 hover:text-white">Strateegia</a>
-    </div>  
+  <img
+    src="https://steamstore-a.akamaihd.net/public/images//gift/steamcards_promo_02.png"
+    alt="Steam gift cards"
+  />
+  <div
+    class="uppercase text-gray-200 flex flex-col text-left ml-24 mt-1 text-sm"
+  >
+    KINKEKAARDID
+    <a href="#" class="text-blue-500 hover:text-white">Nüüd on olemas</a>
+  </div>
+  <div
+    class="uppercase text-gray-200 flex flex-col text-left ml-24 mt-1 text-sm"
+  >
+    SOOVITUSED
+    <a href="#" class="text-blue-500 hover:text-white">Sõprade Poolt</a>
+    <a href="#" class="text-blue-500 hover:text-white">Kuraatorite poolt</a>
+    <a href="#" class="text-blue-500 hover:text-white">Märksõnad</a>
+  </div>
+  <div
+    class="uppercase text-gray-200 flex flex-col text-left ml-24 mt-1 text-sm"
+  >
+    AVASTAMISE JÄRJEKORD
+    <a href="#" class="text-blue-500 hover:text-white">Soovitused</a>
+    <a href="#" class="text-blue-500 hover:text-white">uued väljaanded</a>
+  </div>
+  <div
+    class="uppercase text-gray-200 flex flex-col text-left ml-24 mt-1 text-sm"
+  >
+    KATEGOORIAD
+    <a href="#" class="text-blue-500 hover:text-white">enimmüüdud</a>
+    <a href="#" class="text-blue-500 hover:text-white">uued väljaanded</a>
+    <a href="#" class="text-blue-500 hover:text-white">tulemas</a>
+    <a href="#" class="text-blue-500 hover:text-white">eripakkumised</a>
+    <a href="#" class="text-blue-500 hover:text-white">Virtuaalne reaalsus</a>
+    <a href="#" class="text-blue-500 hover:text-white">kontroller sõbralik</a>
+  </div>
+  <div
+    class="uppercase text-gray-200 flex flex-col text-left ml-24 mt-1 text-sm"
+  >
+    ŽANRID
+    <a href="#" class="text-blue-500 hover:text-white">tasuta mängida</a>
+    <a href="#" class="text-blue-500 hover:text-white">varajane ligipääs</a>
+    <a href="#" class="text-blue-500 hover:text-white">Tegevus</a>
+    <a href="#" class="text-blue-500 hover:text-white">Seiklus</a>
+    <a href="#" class="text-blue-500 hover:text-white">Juhuslik</a>
+    <a href="#" class="text-blue-500 hover:text-white">inde</a>
+    <a href="#" class="text-blue-500 hover:text-white"
+      >Massiivselt mitmikmäng</a
+    >
+    <a href="#" class="text-blue-500 hover:text-white">Võidusõit</a>
+    <a href="#" class="text-blue-500 hover:text-white">RPG</a>
+    <a href="#" class="text-blue-500 hover:text-white">Simulatsioon</a>
+    <a href="#" class="text-blue-500 hover:text-white">Sport</a>
+    <a href="#" class="text-blue-500 hover:text-white">Strateegia</a>
+  </div>
 </template>
