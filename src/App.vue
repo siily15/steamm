0bad0cc24983d47787b9fb172cc4af505d0fc7e7
siily <arnokosk15@gmail.com>
2020-10-19 13:49:23 +0300

    sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
-  <primary-nav />
-  <secondary-nav />
+   <primary-nav />
+  <div class="grid grid-cols-main mt-8 gap-4">
+    <div>
+      <sidebar />
+    </div>
+    <div>
+      <secondary-nav />
+    </div>
+  </div>
 </template>
 <script>
 import PrimaryNav from "./components/PrimaryNav.vue";
 import SecondaryNav from "./components/SecondaryNav.vue";
-
+import sidebar from "./components/sidebar.vue";
 export default {
   name: "App",
   components: {
     PrimaryNav,
     SecondaryNav,
+    sidebar,
   },
 };
 </script>ˇ
--- src/components/SecondaryNav.vue
+++ src/components/SecondaryNav.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="flex flex-1 bg-secondaryBlue m-40 mt-8 bg ">
-    <div class=" px-4 py-2 border-r-2 border-navy-500 hover:bg-blue-800">
+    <div class=" px-4 py-2 border-r-2 border-gray-800 hover:from-gray-400 hover:to-gray-500">
       <span class="text-white ">Sinu Pood</span>
     </div>
-    <div class=" px-4 py-2 border-r-2 border-navy-500 hover:bg-blue-800">
+    <div class=" px-4 py-2 border-r-2 border-gray-800 hover:from-gray-400 hover:to-gray-500">
       <span class="text-white">Sirvi</span>
     </div>
-    <div class=" px-4 py-2 border-r-2 border-navy-500 hover:bg-blue-800">
+    <div class=" px-4 py-2 border-r-2 border-gray-800 hover:from-gray-400 hover:to-gray-500">
       <span class="text-white">Punktide Pood</span>
     </div>
-    <div class=" px-4 py-2 border-r-2 border-navy-500 hover:bg-blue-800">
+    <div class=" px-4 py-2 border-r-2 border-gray-800 hover:from-gray-400 hover:to-gray-500">
       <span class="text-white">Uudised</span>
     </div>
-    <div class=" px-4 py-2 border-r-2 border-navy-500 hover:bg-blue-800 ">
+    <div class=" px-4 py-2 border-r-2 border-gray-800 hover:from-gray-400 hover:to-gray-500">
       <span class="text-white">Labor</span>
     </div>
-    <div class="ml-auto p-1 py-2 border-solid border-2 border-black text-red-100 " >
-      <input class="bg-blue-700" type="text" name="search" placeholder="otsi poest.." />
+    <div class="ml-auto p-2" >
+      <input class="bg-blue-800 bg-blue-800 border-2 border-gray-800 rounded " type="text" name="search" placeholder="otsi poest.." />
     </div>
   </div>
 </template>
